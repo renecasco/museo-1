2a6bb8fb875cbe4abe51db353cf5540a3362f6d0
Rene Casco <rene.casco@icloud.com>
2018-10-31 09:38:32 -0600

    Iteration 1 completed

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -4,4 +4,32 @@ require './lib/artist'
 
 class ArtistTest < Minitest::Test
 
+  def test_it_exists
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+    assert_instance_of Artist, artist
+  end
+
+  def test_it_has_attributes
+    attributes = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist = Artist.new(attributes)
+    assert_equal "2", artist.id
+    assert_equal "Ansel Adams", artist.name
+    assert_equal "1902", artist.born
+    assert_equal "1984", artist.died
+    assert_equal "United States", artist.country
+  end
+
 end
