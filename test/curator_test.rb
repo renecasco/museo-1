23bf7ebb18b6b6f427188ca408765604ffb36e36
Rene Casco <rene.casco@icloud.com>
2018-10-31 10:59:15 -0600

    Iteration 3 last method WIP

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -224,10 +224,71 @@ class CuratorTest < Minitest::Test
     curator.add_artist(artist_2)
     curator.add_artist(artist_3)
     diane_arbus = curator.find_artist_by_id("3")
-    
+
     assert_equal [diane_arbus], curator.artists_with_multiple_photographs
     assert_equal 1, curator.artists_with_multiple_photographs.length
     assert_equal diane_arbus, curator.artists_with_multiple_photographs.first
+  end
+
+  def test_it_can_find_photographs_taken_from_country
+    photo_1 = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    }
+    photo_2 = {
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    }
+    photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    }
+    photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962"
+    }
+    artist_1 = {
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    }
+    artist_2 = {
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    }
+    artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    }
+    curator = Curator.new
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    diane_arbus = curator.find_artist_by_id("3")
+
+    assert_equal 3, curator.photographs_taken_by_artists_from("United States").length
+    assert_equal [], curator.photographs_taken_by_artists_from("Argentina")
+
   end
 
 end
